fa569352f87ca600c4622dbe2716e71657ee087f
Felipe <lamediatricolor@gmail.com>
2026-03-13 12:12:39 -0500

    ft: implementando repositorio de patient

--- backend/src/main/java/com/fepdev/sfm/backend/domain/patient/PatientRepository.java
+++ backend/src/main/java/com/fepdev/sfm/backend/domain/patient/PatientRepository.java
@@ -1,7 +1,35 @@
 package com.fepdev.sfm.backend.domain.patient;
 
+import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
-public interface PatientRepository extends JpaRepository<Patient, UUID> {}
+@Repository
+public interface PatientRepository extends JpaRepository<Patient, UUID> {
+    Optional<Patient> findByDni(String dni);
+    boolean existsByDni(String dni);
+
+    // listar pacientes con filtro por apellido y paginacion
+   @Query("""
+        SELECT p
+        FROM Patient p
+        WHERE (:lastName IS NULL OR LOWER(p.lastName) LIKE LOWER(CONCAT('%', :lastName, '%')))""")
+    Page<Patient> findWithFilters(@Param("lastName") String lastName, Pageable pageable);
+
+    // busqueda rapida por nombre, apellido o dni
+    @Query("""
+        SELECT p 
+        FROM Patient p 
+        WHERE (:searchTerm IS NULL OR 
+               LOWER(p.firstName) LIKE LOWER(CONCAT('%', :searchTerm, '%')) OR 
+               LOWER(p.lastName) LIKE LOWER(CONCAT('%', :searchTerm, '%')) OR 
+               p.dni LIKE CONCAT('%', :searchTerm, '%'))""")
+    List<Patient> quickSearch(@Param("searchTerm") String searchTerm, Pageable pageable);
+}
