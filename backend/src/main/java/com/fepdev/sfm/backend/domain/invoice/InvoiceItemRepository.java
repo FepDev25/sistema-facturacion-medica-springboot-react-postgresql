09df044490fadabafc70f61865cfc1c558685402
Felipe <lamediatricolor@gmail.com>
2026-03-18 10:28:39 -0500

    ft: implementando repositorio de invoice item con metodo para encontrar items por id de invoice

--- backend/src/main/java/com/fepdev/sfm/backend/domain/invoice/InvoiceItemRepository.java
+++ backend/src/main/java/com/fepdev/sfm/backend/domain/invoice/InvoiceItemRepository.java
@@ -1,7 +1,14 @@
 package com.fepdev.sfm.backend.domain.invoice;
 
+import java.util.List;
 import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-public interface InvoiceItemRepository extends JpaRepository<InvoiceItem, UUID> {}
+@Repository
+public interface InvoiceItemRepository extends JpaRepository<InvoiceItem, UUID> {
+
+    // Método personalizado para obtener los items de una factura por su ID
+    List<InvoiceItem> findByInvoiceId(UUID invoiceId);
+}
