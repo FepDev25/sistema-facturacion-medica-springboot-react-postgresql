7ff50e1dc7f46bdd624a09f61113eb5019a5135b
Felipe <lamediatricolor@gmail.com>
2026-03-17 22:09:23 -0500

    ft: implementando repositorio de diagnosis

--- backend/src/main/java/com/fepdev/sfm/backend/domain/medicalrecord/DiagnosisRepository.java
+++ backend/src/main/java/com/fepdev/sfm/backend/domain/medicalrecord/DiagnosisRepository.java
@@ -2,6 +2,16 @@ package com.fepdev.sfm.backend.domain.medicalrecord;
 
 import java.util.UUID;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-public interface DiagnosisRepository extends JpaRepository<Diagnosis, UUID> {}
+
+@Repository
+public interface DiagnosisRepository extends JpaRepository<Diagnosis, UUID> {
+    
+    Page<Diagnosis> findByMedicalRecordId(UUID medicalRecordId, Pageable pageable);
+
+    Page<Diagnosis> findByIcd10Code(String icd10Code, Pageable pageable);
+}
