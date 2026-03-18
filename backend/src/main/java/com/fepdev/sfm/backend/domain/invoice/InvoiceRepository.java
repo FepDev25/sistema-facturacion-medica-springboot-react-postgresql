b7daa3a506e3da7445d2e799dd7388928b4baff7
Felipe <lamediatricolor@gmail.com>
2026-03-18 10:28:54 -0500

    ft: implementando repositorio de invoice aun vacio

--- backend/src/main/java/com/fepdev/sfm/backend/domain/invoice/InvoiceRepository.java
+++ backend/src/main/java/com/fepdev/sfm/backend/domain/invoice/InvoiceRepository.java
@@ -3,5 +3,9 @@ package com.fepdev.sfm.backend.domain.invoice;
 import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-public interface InvoiceRepository extends JpaRepository<Invoice, UUID> {}
+@Repository
+public interface InvoiceRepository extends JpaRepository<Invoice, UUID> {
+
+}
