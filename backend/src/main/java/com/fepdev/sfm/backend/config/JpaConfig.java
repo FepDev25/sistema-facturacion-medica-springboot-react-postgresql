bbfea0af45ef73c5ff30bd3ca6823b1076b0f701
Felipe <lamediatricolor@gmail.com>
2026-03-31 15:01:43 -0500

    ft: DateTimeProvider para auditoría en runtime

--- backend/src/main/java/com/fepdev/sfm/backend/config/JpaConfig.java
+++ backend/src/main/java/com/fepdev/sfm/backend/config/JpaConfig.java
@@ -1,10 +1,19 @@
 package com.fepdev.sfm.backend.config;
 
+import java.time.OffsetDateTime;
+import java.util.Optional;
+
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.auditing.DateTimeProvider;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @Configuration
-@EnableJpaAuditing(auditorAwareRef = "springSecurityAuditorAware")
+@EnableJpaAuditing(auditorAwareRef = "springSecurityAuditorAware", dateTimeProviderRef = "auditingDateTimeProvider")
 public class JpaConfig {
 
+    @Bean("auditingDateTimeProvider")
+    DateTimeProvider auditingDateTimeProvider() {
+        return () -> Optional.of(OffsetDateTime.now());
+    }
 }
